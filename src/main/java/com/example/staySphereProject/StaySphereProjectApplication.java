ea6bd477d9025b53bb2e6f2d0d2c4efc471c2fff
Mergim Januzi <mergim.januzi@gmail.com>
2025-02-14 11:08:33 +0100

    first commit

---
+++ src/main/java/com/example/staySphereProject/StaySphereProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.staySphereProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaySphereProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaySphereProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/staySphereProject/StaySphereProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.staySphereProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaySphereProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
