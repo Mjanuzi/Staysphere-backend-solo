938c44d63e172dfa3736811315f9f0a415a09c53
Mergim Januzi <mergim.januzi@gmail.com>
2025-02-18 17:27:42 +0100

    changed validators for strings from notEmpty to notBlank

--- src/main/java/com/example/staySphereProject/models/Review.java
+++ src/main/java/com/example/staySphereProject/models/Review.java
@@ -1,9 +1,6 @@
 package com.example.staySphereProject.models;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -19,12 +16,12 @@ public class Review {
 
     @DBRef
     @NotNull(message = "User can not be null in a review")
-    @NotEmpty(message = "User can not be empty in a review")
+    @NotBlank(message = "User can not be empty in a review")
     private User userReviewer;
 
     @DBRef
     @NotNull(message = "Listing can not be null in a review")
-    @NotEmpty(message = "Listing can not be empty in a review ")
+    @NotBlank(message = "Listing can not be empty in a review ")
     private Listing listingReviewed;
 
     @Max(value = 500, message = "Review can not have more than 500 characters")
