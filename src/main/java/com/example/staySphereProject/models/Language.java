727d9044746eb833410d127cacafb2f7bb341ae4
Mergim Januzi <mergim.januzi@gmail.com>
2025-02-19 11:17:35 +0100

    language enums set

---
+++ src/main/java/com/example/staySphereProject/models/Language.java
@@ -0,0 +1,16 @@
+package com.example.staySphereProject.models;
+
+public enum Language {
+    ENGLISH,
+    SWEDISH,
+    SPANISH,
+    DANISH,
+    FINISH,
+    RUSSIAN,
+    NORWEGIAN,
+    ITALIAN,
+    FRENCH,
+    GERMAN,
+    PORTUGUESE,
+    DUTCH;
+}
