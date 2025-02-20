885cdd2a6bd5bffa42ec2ea5edf587da3965a2b1
Larsin15 <tommy.larsin@hotmail.com>
2025-02-20 11:25:53 +0100

    BookingsResponse in dto

---
+++ src/main/java/com/example/staySphereProject/dto/BookingsResponse.java
@@ -0,0 +1,18 @@
+package com.example.staySphereProject.dto;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class BookingsResponse {
+
+    private String bookingID;
+    private String userId;
+    private String listingId;
+    private String bookingName;
+    private LocalDateTime bookingDate;
+    private Date startDate;
+    private Date endDate;
+    private double totalCost;
+    private boolean status;
+    private boolean isPending;
+}
