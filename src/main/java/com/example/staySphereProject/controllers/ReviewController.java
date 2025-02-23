95927aa7c96b123fd3ce10e21ba9b68a32b1e11d
Mergim Januzi <mergim.januzi@gmail.com>
2025-02-23 21:29:41 +0100

    started controller and service for reviews with constructors

---
+++ src/main/java/com/example/staySphereProject/controllers/ReviewController.java
@@ -0,0 +1,16 @@
+package com.example.staySphereProject.controllers;
+
+import com.example.staySphereProject.services.ReviewService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+}
---
+++ src/main/java/com/example/staySphereProject/services/ReviewService.java
@@ -0,0 +1,20 @@
+package com.example.staySphereProject.services;
+
+import com.example.staySphereProject.repository.ListingRepository;
+import com.example.staySphereProject.repository.ReviewRepository;
+import com.example.staySphereProject.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+    private final ListingRepository listingRepository;
+
+    public ReviewService(ReviewRepository reviewRepository, UserRepository userRepository, ListingRepository listingRepository) {
+        this.reviewRepository = reviewRepository;
+        this.userRepository = userRepository;
+        this.listingRepository = listingRepository;
+    }
+}
