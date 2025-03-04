00e79ad748b801cf871f66331a76696d275c24ed
Larsin15 <tommy.larsin@hotmail.com>
2025-03-04 12:42:16 +0100

    Logout function added and tested in postman

--- src/main/java/com/example/staySphereProject/controllers/AuthController.java
+++ src/main/java/com/example/staySphereProject/controllers/AuthController.java
@@ -123,6 +123,29 @@ public class AuthController {
                     .status(HttpStatus.UNAUTHORIZED)
                     .body("Incorrect username or password");
         }
+
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletResponse response) {
+        // skapa en utgången cookie för att ersätta den befintliga jwt cookien
+        ResponseCookie jwtCookie = ResponseCookie.from("jwt", "")
+                .httpOnly(true)
+                .secure(false) // VIKTIGT! ändra i production
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+        // response.addHeader(HttpHeaders.SET_COOKIE, jwtCookie.toString());
+
+        // rensa securitykontexten
+        SecurityContextHolder.clearContext();
+
+        // returnera svar med utgången cookie
+        return ResponseEntity.ok()
+                // hade kunnat ta bort denna raden och använda rad 134
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body("Logout successful!");
     }
 
 }
