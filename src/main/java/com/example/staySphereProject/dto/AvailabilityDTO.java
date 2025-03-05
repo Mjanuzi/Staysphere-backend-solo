0e59596a96f56fa1f43d4fbc393c25a55f03547c
Larsin15 <tommy.larsin@hotmail.com>
2025-03-05 12:13:04 +0100

    First commit to available dates

---
+++ src/main/java/com/example/staySphereProject/dto/AvailabilityDTO.java
@@ -0,0 +1,29 @@
+package com.example.staySphereProject.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public class AvailabilityDTO {
+    @NotNull
+    private LocalDate startDate;
+
+    @NotNull
+    private LocalDate endDate;
+
+    public @NotNull LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(@NotNull LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public @NotNull LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(@NotNull LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
