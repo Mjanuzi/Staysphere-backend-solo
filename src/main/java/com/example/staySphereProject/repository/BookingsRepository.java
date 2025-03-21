05366e415f02d34ff8f45cad344ac5886b345517
Mergim Januzi <mergim.januzi@gmail.com>
2025-03-21 12:02:05 +0100

    validate dates and users before making a review

--- src/main/java/com/example/staySphereProject/repository/BookingsRepository.java
+++ src/main/java/com/example/staySphereProject/repository/BookingsRepository.java
@@ -4,11 +4,15 @@ import com.example.staySphereProject.models.Bookings;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookingsRepository extends MongoRepository<Bookings, String> {
 
     List<Bookings> findByUserId(String userId);
 
     List<Bookings> findByListingIdIn(List<String> listingIds);
+
+    // Hämta booking baserat på användare och listing
+    Optional<Bookings> findByUserIdAndListingId(String userId, String listingId);
 
 }
