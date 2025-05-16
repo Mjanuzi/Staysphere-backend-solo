73557e5ddc7c5ecefda612845d1bbbf7bc98c9f6
Larsin15 <tommy.larsin@hotmail.com>
2025-05-16 17:19:30 +0200

    Implement Patch in cors

--- src/main/java/com/example/staySphereProject/config/SecurityConfig.java
+++ src/main/java/com/example/staySphereProject/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cookie"));
         configuration.setExposedHeaders(Arrays.asList("Set-Cookie"));
         configuration.setAllowCredentials(true);
