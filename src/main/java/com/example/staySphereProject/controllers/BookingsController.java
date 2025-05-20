f6b63eb73aa34e7a2473c067c962e0ec6dc74235
Larsin15 <tommy.larsin@hotmail.com>
2025-05-20 16:34:03 +0200

    making sure are endpoints start with /api/

--- src/main/java/com/example/staySphereProject/controllers/BookingsController.java
+++ src/main/java/com/example/staySphereProject/controllers/BookingsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bookings")
+@RequestMapping("/api/bookings")
 public class BookingsController {
     private final BookingsService bookingsService;
 
