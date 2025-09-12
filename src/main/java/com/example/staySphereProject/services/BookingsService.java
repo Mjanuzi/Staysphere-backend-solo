8cde6d6bcef8ae3b10152922e0d1f10e5ace34cb
Mergim Januzi <mergim.januzi@gmail.com>
2025-09-12 14:56:21 +0200

    checkauth working in createbooking

--- src/main/java/com/example/staySphereProject/services/BookingsService.java
+++ src/main/java/com/example/staySphereProject/services/BookingsService.java
@@ -191,14 +191,14 @@ public class BookingsService {
     @Transactional
     public BookingsResponse createBooking(BookingsDTO bookingsDTO) {
 
-        //checkAuthentication.validateAuthenticatedUser(bookingsDTO.getUserId());
+        checkAuthentication.validateAuthenticatedUser(bookingsDTO.getUserId());
 
-        if (!userRepository.existsById(bookingsDTO.getUserId())) {
+       /* if (!userRepository.existsById(bookingsDTO.getUserId())) {
             throw new ResourceNotFoundException("User not found");
         }
         if (!listingRepository.existsById(bookingsDTO.getListingId())) {
             throw new ResourceNotFoundException("Listing not found");
-        }
+        }*/
 
         Listing listing = listingRepository.findById(bookingsDTO.getListingId())
                 .orElseThrow(() -> new ResourceNotFoundException("Listing not found"));
@@ -251,6 +251,8 @@ public class BookingsService {
 
         Listing listing = listingRepository.findById(existingBooking.getListingId())
                 .orElseThrow(() -> new ResourceNotFoundException("Listing not found"));
+
+        checkAuthentication.validateAuthenticatedUser(bookingsDTO.getUserId());
 
         List<LocalDate> originalDates = existingBooking.getBookedDates();
 
