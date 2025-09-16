9cb2184ffe42a51773a698a44c861eb8df3f143a
Mergim Januzi <mergim.januzi@gmail.com>
2025-09-16 16:39:58 +0200

    started with the template mthod hooks for listing

--- src/main/java/com/example/staySphereProject/models/Residence.java
+++ src/main/java/com/example/staySphereProject/models/Residence.java
@@ -1,19 +1,24 @@
 package com.example.staySphereProject.models;
 
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Residence")
+@Document(collection = "listings")
 public class Residence extends Listing {
 
+
+
     @DBRef
+    @NotNull(message = "Residence must have an host")
     private User host;
 
-
-    public Residence() {
-        super();
-
+    public @NotNull(message = "Residence must have an host") User getHost() {
+        return host;
     }
 
+    public void setHost(@NotNull(message = "Residence must have an host") User host) {
+        this.host = host;
+    }
 }
