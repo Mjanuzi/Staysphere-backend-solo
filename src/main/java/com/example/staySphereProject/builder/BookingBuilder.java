0b697066740f5defa4f31a270003fa47c703c39b
Larsin15 <tommy.larsin@hotmail.com>
2025-09-19 14:33:25 +0200

    Start of the bookingBuilder with constructor

---
+++ src/main/java/com/example/staySphereProject/builder/BookingBuilder.java
@@ -0,0 +1,56 @@
+package com.example.staySphereProject.builder;
+
+import com.example.staySphereProject.converters.BookingDTOConverter;
+import com.example.staySphereProject.dto.BookingsDTO;
+import com.example.staySphereProject.models.Listing;
+import com.example.staySphereProject.models.User;
+import com.example.staySphereProject.repository.ListingRepository;
+import com.example.staySphereProject.services.AvailabilityService;
+import com.example.staySphereProject.services.CostCalculationService;
+import com.example.staySphereProject.services.DateRangeService;
+import com.example.staySphereProject.services.UserService;
+import com.example.staySphereProject.util.CheckAuthentication;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+public class BookingBuilder {
+
+    //Immutable dependdencies
+    private final BookingDTOConverter converter;
+    private final CostCalculationService costCalculationService;
+    private final DateRangeService dateRangeService;
+    private final AvailabilityService availabilityService;
+    private final UserService userService;
+    private final ListingRepository listingRepository;
+    private final CheckAuthentication checkAuthentication;
+
+    //Mutable
+    private BookingsDTO bookingsDTO;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private List<LocalDate> requestDates;
+    private User user;
+    private Listing listing;
+    private double totalCost;
+    private boolean validated;
+
+
+    public BookingBuilder(BookingDTOConverter converter,
+                          CostCalculationService costCalculationService,
+                          DateRangeService dateRangeService,
+                          AvailabilityService availabilityService,
+                          UserService userService,
+                          ListingRepository listingRepository,
+                          CheckAuthentication checkAuthentication) {
+        this.converter = converter;
+        this.costCalculationService = costCalculationService;
+        this.dateRangeService = dateRangeService;
+        this.availabilityService = availabilityService;
+        this.userService = userService;
+        this.listingRepository = listingRepository;
+        this.checkAuthentication = checkAuthentication;
+    }
+}
